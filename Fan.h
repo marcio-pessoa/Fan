187970edc4ff35167e1d05397bde11488c3d4aa6
Márcio Pessoa <marcio.pessoa@sciemon.com>
2018-10-17 17:08:18 -0300

    Minor updates

--- Fan.h
+++ Fan.h
@@ -1,6 +1,6 @@
 /* Fan.h - Fan Speed Control and Sensor Library
  * 
- * Author: Márcio Pessoa <marcio.pessoa@sciemon.com>
+ * Author: Márcio Pessoa <marcio.pessoa@gmail.com>
  * Contributors: none
  * 
  * Change log
